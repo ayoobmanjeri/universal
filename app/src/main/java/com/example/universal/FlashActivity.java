2d7185344c994b3ada54f51fe68e37753fac8073
ayoobkp <ayoobmanjeri@gmail.com>
2020-06-15 12:08:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/universal/FlashActivity.java
@@ -0,0 +1,14 @@
+package com.example.universal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
